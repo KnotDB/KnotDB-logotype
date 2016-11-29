925d14ea43c1aa3361db2651b6bd53e06b7b70df
Sequomics Research <SequomicsResearch@users.noreply.github.com>
2016-11-29 13:48:33 +0530

    Initial commit
    Required module — load-grunt-tasks » https://github.com/sindresorhus/load-grunt-tasks
    Usage: $ npm install load-grunt-tasks --save-dev

--- Gruntfile.js
+++ Gruntfile.js
@@ -106,3 +106,10 @@ module.exports = function(grunt) {
   // Date: 29-11-2016.
   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   grunt.initConfig(config);
+  
+  // 2. load-grunt-tasks ——> $ npm install load-grunt-tasks --save-dev
+  // -----------------------------------------------------------------
+  // Load multiple grunt tasks using globbing patterns.
+  require('load-grunt-tasks')(grunt, {
+    scope: ['devDependencies', 'dependencies']
+  });
