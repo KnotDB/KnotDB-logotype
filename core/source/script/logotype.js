b103a9ee980eb66e75d1d17dd79b841f49d12147
Sequomics Research <SequomicsResearch@users.noreply.github.com>
2016-11-29 18:02:21 +0530

    Initial commit
    A main LogoType JavaScript of KnotDB®.

---
+++ core/source/script/logotype.js
@@ -0,0 +1,24 @@
+/*!
+ * ----------------
+ * KnotDB®-logotype
+ * ----------------
+ * LogoType of KnotDB® — A manually curated dedicated Protein Knots database.
+ * ___________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Sequømics Research [http://research.sequomics.com/].
+ * @copyright : Sequømics Corporation [http://sequomics.com/].
+ * ___________________________________________________________________________
+ *
+ * @date      : 29-Nov-2016
+ * @license   : Apache, version 2.0
+ * @require   : d3.js®
+ * @require   : jQuery
+ * @require   : Modernizr
+ * @build     : SEED™ — Umeå
+ *              └────── A Sequømics Product — http://sequomics.com/.
+ * ___________________________________________________________________________
+ */
