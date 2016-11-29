fd4f8c952ba0e5142635e206ae900e45e3717fc7
Sequomics Research <SequomicsResearch@users.noreply.github.com>
2016-11-29 12:09:00 +0530

    Initial commit
    Usage: $ node -v | $ npm -v | $ grunt -version

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,3 +29,8 @@
  * --/The Heart of Build System/-- of "KnotDB®-logotype".
  * ___________________________________________________________________________
  */
+
+
+// # Usage: $ node -v
+// # Usage: $ npm -v
+// # Usage: $ grunt -version
