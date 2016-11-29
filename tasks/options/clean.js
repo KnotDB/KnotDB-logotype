e87099d572a61f9f89c86d1dc5f6386cea068a22
Sequomics Research <SequomicsResearch@users.noreply.github.com>
2016-11-29 16:18:24 +0530

    Initial commit
    Apache License | Build with SEED™ — Umeå.

---
+++ tasks/options/clean.js
@@ -0,0 +1,30 @@
+/*!
+ * ----------------
+ * KnotDB®-logotype
+ * ----------------
+ * LogoType of KnotDB® — A manually curated dedicated Protein Knots database.
+ * ___________________________________________________________________________
+ *
+ * Grunt, http://gruntjs.com/ — The JavaScript Task Runner.
+ * ___________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Sequømics Research [http://research.sequomics.com/].
+ * @copyright : Sequømics Corporation [http://sequomics.com/].
+ * ___________________________________________________________________________
+ *
+ * @date      : 29-Nov-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt-contrib-clean
+ * @build     : SEED™ — Umeå
+ *              └────── A Sequømics Product — http://sequomics.com/.
+ * ___________________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "KnotDB®-logotype".
+ * ___________________________________________________________________________
+ */
