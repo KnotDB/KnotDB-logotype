b821f3afd7c1d8786f2c01e588b99729f118cb6d
Sequomics Research <SequomicsResearch@users.noreply.github.com>
2016-11-29 16:04:32 +0530

    Initial commit
    Registering a smart banner function.

--- Gruntfile.js
+++ Gruntfile.js
@@ -117,6 +117,23 @@ module.exports = function(grunt) {
   // To get 'version', i.e. required to work on SEED™: Grunt based build system.
   var version = process.env.VERSION || require('./package.json').version;
   
+  // A smart banner function.
+  ///-----------------------
+  var banner = [
+    '/*!                                                                                                  ',
+    ' * Build System — ' + yeep(build.system) + ': ' + okay(build.name) + ' — ' + version                  ,
+    ' * ' + noop(build.audience)                                                                           ,
+    ' * ---------------------------------------------------------------------------                       ',
+    ' * Copyright © 2015 - ' + new Date().getFullYear() + ', Sequømics Corporation, All rights reserved.  ',
+    ' * Available via the Apache, version 2.0. [http://www.apache.org/licenses/]                          ',
+    ' * See: http://seed.sequomics.com/ — for details.                                                    ',
+    ' * ---------------------------------------------------------------------------                       ',
+    ' */                                                                                                  ',
+    '\n',
+  ].map(function(s) {
+    return s.replace(/\s+$/, '');
+  }).join("\n");
+  
   // A task to clean build(s) or Temporary file(s) and default cache directories.
   grunt.registerTask('clear', ['clean']);
   
