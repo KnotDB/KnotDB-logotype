a245f5058e76a68eb9c1636f356d3747500dff29
Sequomics Research <SequomicsResearch@users.noreply.github.com>
2016-11-29 11:58:04 +0530

    Initial commit
    build.sh, shell script to --/build/-- whole "KnotDB®-logotype" in one go.
    Usage: $ sh build.sh

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# ====================++== KnotDB®-logotype ==++======================
+#        Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+#     Copyright © 2006 - 2016, Prabhat Kumar. All rights reserved.
+# ====================================================================
